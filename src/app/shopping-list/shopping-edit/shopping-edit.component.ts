76c6f177a33a74b87fc649afc8cf3985806ff11c
Thomas Elliott <thomaselliott@outlook.com>
2018-07-05 18:56:02 +1200

    Add databinding to shopping component

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, Output, ViewChild} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent {
+  @ViewChild('nameInput') name: ElementRef;
+  @ViewChild('amountInput') amount: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  onClickAdd() {
+    const ingredient = new Ingredient(this.name.nativeElement.value, this.amount.nativeElement.value);
 
-  ngOnInit() {
+    this.ingredientAdded.emit(ingredient);
   }
-
 }
